670384fb60449e3d5060b7d537fba70755118ea1
MIGHTY\opana <opanagushin@gmail.com>
2017-05-04 17:20:04 +0000

    add support of vs 2017

---
+++ Source/NFX.VisualStudio/2017/NhtTaggerProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace NFX.VisualStudio
+{
+  [ContentType(Constants.NFX)]
+  [TagType(typeof(IClassificationTag))]
+  [TagType(typeof(IErrorTag))]
+  [Export(typeof(ITaggerProvider))]
+  internal sealed class NhtTaggerProvider : BaseTaggerProvider, ITaggerProvider
+  {
+    [Export]
+    [BaseDefinition("htmlx")]
+    [Name(Constants.NFX)]
+    internal static ContentTypeDefinition NfxContentType { get; set; }
+
+    [Export]
+    [FileExtension(".nht")]
+    [ContentType(Constants.NFX)]
+    internal static FileExtensionToContentTypeDefinition NfxFileType { get; set; }
+
+    [Import]
+    internal IClassificationTypeRegistryService ClassificationTypeRegistry { get; set; }
+
+    [Import]
+    internal IBufferTagAggregatorFactoryService TagAggregatorFactoryService { get; set; }
+
+    [Import]
+    internal ITextBufferFactoryService BufferFactory { get; set; }
+
+    [Import]
+    internal IContentTypeRegistryService ContentTypeRegistryService { get; set; }
+
+    public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+    {
+      return GetTagger<T>(buffer, (docName) => { return new NhtTagger(ClassificationTypeRegistry, BufferFactory, TagAggregatorFactoryService, ContentTypeRegistryService, docName); });
+    }
+  }
+}
